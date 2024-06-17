29f57ce69e6f85b16ebc5376f9d33bed019df503
ariazero <aryaabadian@gmail.com>
2024-06-17 14:36:20 +0330

    build the color mode switch

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,13 +1,14 @@
-import { HStack, Image, Text } from "@chakra-ui/react"
-import logo from '../assets/logo.webp'
+import { HStack, Image } from "@chakra-ui/react";
+import logo from "../assets/logo.webp";
+import ColorModeSwitch from "./ColorModeSwitch.tsx";
 
 const NavBar = () => {
   return (
-    <HStack>
-        <Image src={logo} boxSize='50px'/>
-        <Text>NavBar</Text>
+    <HStack justifyContent='space-between' padding='10px'>
+      <Image src={logo} boxSize="50px" />
+      <ColorModeSwitch />
     </HStack>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
